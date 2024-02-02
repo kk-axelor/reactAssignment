ada0eb14aa42934d692f1b33fd242e90a250a551
ksh-axelor <k.kumarshaw@axelor.com>
2024-02-02 12:31:59 +0530

    update the  css to module css and remove console log

--- src/Components/CalenderView.jsx
+++ src/Components/CalenderView.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useId, useState } from "react";
 import { weeeks, months } from "../constant";
 
-import "./calenderView.css";
+import styles from "./calenderView.module.css";
 const CalenderView = () => {
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
@@ -22,45 +22,28 @@ const CalenderView = () => {
     );
   }, [currentMonth, currentYear]);
 
-  const handlePrev = () => {
-    if (currentMonth === 0) {
-      setCurrentMonth(11);
-      setCurrentYear(currentYear - 1);
-    } else {
-      setCurrentMonth(currentMonth - 1);
-    }
-  };
-  const handleNext = () => {
-    if (currentMonth === 11) {
-      setCurrentMonth(0);
-      setCurrentYear(currentYear + 1);
-    } else {
-      setCurrentMonth(currentMonth + 1);
-    }
-  };
-  const handleClick = (type, month) => {
+  const handleClick = (type) => {
     if (type === "Prev") {
-      setCurrentMonth(currentMonth === 0 ? month : currentMonth - 1);
-      setCurrentYear(currentYear - 1);
+      setCurrentMonth(currentMonth === 0 ? currentMonth : currentMonth - 1);
+      setCurrentYear(currentMonth === 0 ? currentYear - 1 : currentYear);
     } else {
       setCurrentMonth(currentMonth === 11 ? 0 : currentMonth + 1);
-      setCurrentYear(currentYear + 1);
+      setCurrentYear(currentMonth === 11 ? currentYear + 1 : currentYear);
     }
   };
-  // handleClick("Prev", 11)
   return (
-    <div className="container">
-      <div className="calender-header">
-        <button onClick={handlePrev}>Prev</button>
+    <div className={styles.container}>
+      <div className={styles.calenderHeader}>
+        <button onClick={() => handleClick("Prev")}>Prev</button>
         <p>
           {months[currentMonth]}-{currentYear}
         </p>
 
-        <button onClick={handleNext}>Next</button>
+        <button onClick={() => handleClick("Next")}>Next</button>
       </div>
-      <ul className="showDates">
+      <ul className={styles.showDates}>
         {weeeks.map((item, i) => (
-          <li key={i} className="list">
+          <li key={i} className={styles.list}>
             {item}
           </li>
         ))}
@@ -68,7 +51,7 @@ const CalenderView = () => {
         <div>
           {list.map((day, i) => (
             <>
-              <li className="list" key={i}>
+              <li className={styles.list} key={i}>
                 {day - firstDay <= 0 ? <span>&nbsp;</span> : day - firstDay}
               </li>
               {day % 7 === 0 && <br key={brID} />}
